28b270e244b5ca747d0b779d73b3016b323e47cd
lalamiffy <lalamiffy2@gmail.com>
2024-11-21 14:37:57 +1100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-//import HeroSection from "./components/HeroSection";
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
